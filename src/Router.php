5893c12758a82ce9b44aa87e9542382270243c64
TechnoPreacher <nik.nik.sulima@gmail.com>
2022-05-09 23:25:31 +0300

    router

--- src/Router.php
+++ src/Router.php
@@ -7,7 +7,14 @@ use Aigletter\Contracts\Routing\RouteInterface;
 class Router implements RouteInterface
 {
 
+
     protected array $routes=[];
+
+
+    public function __construct(string $path="", $action="")
+    {
+        $this->addRoute($path,$action);
+    }
 
     public function route(string $uri): callable
     {
