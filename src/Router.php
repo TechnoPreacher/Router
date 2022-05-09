9734759aa2466481d7186982c1fcda48377f984c
TechnoPreacher <nik.nik.sulima@gmail.com>
2022-05-09 17:32:04 +0300

    router

---
+++ src/Router.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Ns\Router;
+
+use Aigletter\Contracts\Routing\RouteInterface;
+
+class Router implements RouteInterface
+{
+
+    protected array $routes;
+
+    public function route(string $uri): callable
+    {
+        foreach ($this->routes as $k => $v) {
+            if (0 == strcmp($uri, $k)) {
+                return $v;
+            }
+        }
+        return throw new \Exception("no action found for $uri ");
+    }
+
+    public function addRoute(string $path, $action): void
+    {
+        $this->routes[$path] = $action;
+    }
+
+}
